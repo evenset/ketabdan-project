27ecec699ed5d850f4bd4e01a12a47b0a52032a2
hesam <hesamd@evenset.com>
2018-05-02 13:46:29 -0400

    Added a few fields to podcosts app

--- podcasts/models.py
+++ podcasts/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+
+class Podcast(models.Model):
+    name = models.CharField(max_length=200, unique=True, help_text="Name of the podcast")
+    description = models.CharField(max_length=1000, blank=True, help_text="A short description of the podcast")
+    avatar = models.ImageField(upload_to='podcasts/avatars/', null=True)
+    itunes_link = models.URLField(blank=True, help_text="The link to your iTunes channel")
+    blubrry_link = models.URLField(blank=True, help_text="The link to you Blubrry channel")
+
+
+class Episode(models.Model):
+    podcast = models.ForeignKey(Podcast, on_delete=models.CASCADE)
+    title = models.CharField(max_length=200, help_text="Title of your episode")
+    story = models.TextField(blank=True)
+    file = models.FileField(upload_to='podcasts/episodes/files/')
+    avatar = models.ImageField(upload_to='podcasts/episodes/')
+    release_date = models.DateTimeField(auto_now=True, editable=False)
+    soundcloud_link = models.URLField(blank=True, help_text="The link to the episode soundcloud")
+
+
+
+
+
