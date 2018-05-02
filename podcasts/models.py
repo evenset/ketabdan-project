d2c1335ea0b348645b5606751af69b8f7ba7c712
hesam <hesamd@evenset.com>
2018-05-02 15:06:45 -0400

    Adding str functions for better model represenatation

--- podcasts/models.py
+++ podcasts/models.py
@@ -8,15 +8,21 @@ class Podcast(models.Model):
     itunes_link = models.URLField(blank=True, help_text="The link to your iTunes channel")
     blubrry_link = models.URLField(blank=True, help_text="The link to you Blubrry channel")
 
+    def __str__(self):
+        return self.name
+
 
 class Episode(models.Model):
     podcast = models.ForeignKey(Podcast, on_delete=models.CASCADE)
     title = models.CharField(max_length=200, help_text="Title of your episode")
     story = models.TextField(blank=True)
     file = models.FileField(upload_to='podcasts/episodes/files/')
-    avatar = models.ImageField(upload_to='podcasts/episodes/')
+    avatar = models.ImageField(upload_to='podcasts/episodes/', null=True)
     release_date = models.DateTimeField(auto_now=True, editable=False)
     soundcloud_link = models.URLField(blank=True, help_text="The link to the episode soundcloud")
+
+    def __str__(self):
+        return self.title
 
 
 
