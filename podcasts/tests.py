596de465e5c8842a9158b6aafd4bc482aa9de444
hesam <hesamd@evenset.com>
2018-05-02 15:26:06 -0400

    Added a dummy unit test

--- podcasts/tests.py
+++ podcasts/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Podcast, Episode
+
+
+class PodcastModelTests(TestCase):
+
+    def test_dummy(self):
+        self.assertIs(True, True)
+
+
