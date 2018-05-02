647d507c7d310f81b16af4a310a828117b2ebdad
hesam <hesamd@evenset.com>
2018-05-02 15:47:06 -0400

    Updated travis

--- ketabdan/settings.py
+++ ketabdan/settings.py
@@ -88,6 +88,19 @@ DATABASES = {
     }
 }
 
+# Travis integration
+if 'TRAVIS' in os.environ:
+    DATABASES = {
+        'default': {
+            'ENGINE':   'django.db.backends.postgresql_psycopg2',
+            'NAME':     'travisci',
+            'USER':     'postgres',
+            'PASSWORD': '',
+            'HOST':     'localhost',
+            'PORT':     '',
+        }
+    }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
