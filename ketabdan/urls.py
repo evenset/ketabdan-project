23b5c85b85ba30fd8a3f286f68e972713a043a43
hesamd <hesamd@gmail.com>
2018-06-27 22:48:08 -0400

    User authentication (#21)
    *  Added social authentication and registration to ketabdan
    
    *  Added social authentication and registration to ketabdan
    
    * temprorily login template
    
    * templates/account/login.html
    
    * Added registeration form
    
    * fixed a name issue on password form and update the link

--- ketabdan/urls.py
+++ ketabdan/urls.py
@@ -17,9 +17,11 @@ from django.contrib import admin
 from django.urls import path, include
 from staticpages.views import IndexView
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('podcasts/', include('podcasts.urls')),
+    path('accounts/', include('allauth.urls')),
     path('', IndexView.as_view())
 ]
 
