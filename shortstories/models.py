7ef75f64565d7ccc73f89a483e9102527d540dee
Your Name <you@example.com>
2018-07-07 23:13:00 +1000

    creating Model

--- shortstories/models.py
+++ shortstories/models.py
@@ -1,3 +1,12 @@
 from django.db import models
-
-# Create your models here.
+class ShortStory(models.Model):
+     status=(
+        ('dr', 'Draft'),
+        ('p' , 'Published'),
+        ('b' , 'banned'),
+        ('de', 'deleted'),
+    )
+    author =models.Foreignkey(user,on_delete=models.CASCADE)
+    title = models.CharField(max_length=500)
+    body = models.CharField(min_length=3500,Choices = status)
+    publication_date = models.DateTimeField('date published')
