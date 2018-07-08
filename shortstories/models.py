661c21a660781d8173a5b7b2db5b9d9efd8c9d0d
Your Name <you@example.com>
2018-07-08 11:28:42 +1000

    Edit Model

--- shortstories/models.py
+++ shortstories/models.py
@@ -1,12 +1,14 @@
 from django.db import models
 class ShortStory(models.Model):
-     status=(
+     PUBLICATION_STATUS =(
         ('dr', 'Draft'),
         ('p' , 'Published'),
-        ('b' , 'banned'),
-        ('de', 'deleted'),
+        ('b' , 'Banned'),
+        ('de', 'Deleted'),
     )
-    author =models.Foreignkey(user,on_delete=models.CASCADE)
-    title = models.CharField(max_length=500)
-    body = models.CharField(min_length=3500,Choices = status)
-    publication_date = models.DateTimeField('date published')
+    user = models.Foreignkey(settings.AUTH_USER_ MODEL)
+    author = models.Foreignkey(user,on_delete = models.CASCADE)
+    title = models.CharField(max_length = 500)
+    body = models.CharField(min_length = 3500)
+    status = models.CharField(max_length = 1, choices = PUBLICATION_STATUS)
+    publication_date = models.DateField()
