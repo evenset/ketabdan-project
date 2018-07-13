dad56781c6d25c171c8143fdc68e740506a65210
Your Name <you@example.com>
2018-07-13 11:51:03 +1000

    Create Migration

--- shortstories/models.py
+++ shortstories/models.py
@@ -1,15 +1,14 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User 
 
 class ShortStory(models.Model):
-     PUBLICATION_STATUS =(
+    PUBLICATION_STATUS =(
         ('dr', 'Draft'),
         ('p' , 'Published'),
         ('b' , 'Banned'),
         ('de', 'Deleted'),
     )
-    author = models.Foreignkey(User,on_delete = models.CASCADE)
+    author = models.ForeignKey(User,on_delete = models.CASCADE) 
     title = models.CharField(max_length = 500)
-    body = models.CharField(min_length = 3500)
     status = models.CharField(max_length = 1, choices = PUBLICATION_STATUS)
     publication_date = models.DateField()
