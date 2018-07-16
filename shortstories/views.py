85cd7461d48d1b2b50111e0f773702f17e6609f0
Your Name <you@example.com>
2018-07-16 13:48:42 +1000

    Index View

--- shortstories/views.py
+++ shortstories/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+def index(request):
+    return  HttpResponse('Author,Title,Status,Publication_Date')
 
-# Create your views here.
+
+
