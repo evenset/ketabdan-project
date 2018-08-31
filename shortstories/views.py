8306bf38e4424015d6d36ba5f02c64e6fc76889a
Your Name <you@example.com>
2018-08-31 13:05:00 +1000

    Changes in Template

--- shortstories/views.py
+++ shortstories/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from .models import ShortStory 
 
 def index(request):
-    shortstory_list = ShortStory.objects.all()
-    context = {"short_story" : shortstory_list}
+    shortstory_list = ShortStory.objects.order_by('publication_date')
+    context = {"shortstories" : shortstory_list}
     return render(request, 'shortstories/index.html', context)
