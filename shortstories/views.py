9467a6a3217ea25ea44e3468fd4bfb720b91e6bf
Your Name <you@example.com>
2018-09-01 18:12:10 +1000

    Showing ShortStories

--- shortstories/views.py
+++ shortstories/views.py
@@ -4,4 +4,4 @@ from .models import ShortStory
 def index(request):
     shortstory_list = ShortStory.objects.order_by('publication_date')
     context = {"shortstories" : shortstory_list}
-    return render(request, 'shortstories/index.html', context)
+    return render(request, 'shortstories/index.html',context)
