d07273cffbde1c34fe71b958b97f081013aa6f7a
Your Name <you@example.com>
2018-09-06 22:51:50 +1000

    Fixing identation, Add Variables in Template, Add new line to the end of the files.

--- shortstories/tests.py
+++ shortstories/tests.py
@@ -2,15 +2,19 @@ from django.test import TestCase
 from .models import ShortStory
 from django.urls import reverse
 from shortstories.models import User
+
+
 def create_shortstories(author, status,title,publication_date):
     return ShortStory.objects.create(author=author, status=status , title=title, publication_date = publication_date)
 
-    
+
 class shortstoriesIndexViewTests(TestCase):
     def  test_no_shortstories(self):
         response = self.client.get(reverse('shortstories:index'))
         self.assertEqual(response.status_code, 200)
-        self.assertContains(response, "No ShortStories availabale")
+        self.assertContains(response, "No short stories available")
+
+
 class shortstoriesDetailViewTests(TestCase):
     def  test_current_shortstories(self):
         user_id = User.objects.create(id=1)
